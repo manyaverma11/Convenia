730b467c90742610d3e44f54a9f49007cc2a7b62
Manya <manyaverma0154@gmail.com>
2024-11-07 19:08:45 +0530

    changed login signup page route path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login-sign-up" element={<LoginPage />} />
       </Routes>
     </Router>
   );
