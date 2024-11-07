35fba3355f5b6ba25db024e1fb2c82a14ff8a8cb
Manya <manyaverma0154@gmail.com>
2024-11-07 19:06:42 +0530

    Login/Signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import HomePage from './Pages/HomePage.jsx'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./Pages/HomePage.jsx";
+import LoginPage from "./Pages/LoginPage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <HomePage />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
