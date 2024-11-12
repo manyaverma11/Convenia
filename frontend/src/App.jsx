7bc9f2f9eb25a048f9b38b8883dde8a7cc6e8eae
Manya <manyaverma0154@gmail.com>
2024-11-12 19:05:54 +0530

    webrtc code working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import HomePage from "./Pages/HomePage.jsx";
-import LoginPage from "./Pages/LoginPage.jsx";
+// import { useState } from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import "./App.css";
+// import HomePage from "./Pages/HomePage.jsx";
+// import LoginPage from "./Pages/LoginPage.jsx";
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login-sign-up" element={<LoginPage />} />
-      </Routes>
-    </Router>
-  );
-}
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/login-sign-up" element={<LoginPage />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
 
-export default App;
+// export default App;
--- server/server.js
+++ server/server.js
@@ -1,32 +1,64 @@
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+const app = express();
+
+const PORT = process.env.PORT || 3030;
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+const io = require("socket.io")(server);
 const { ExpressPeerServer } = require("peer");
+const url = require("url");
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
 const path = require("path");
 
-const app = express();
-const server = http.Server(app);
-const io = new Server(server);
-const peerServer = ExpressPeerServer(server, { debug: true });
-const PORT = process.env.PORT || 3000;
-
-// Middleware
+app.set("view engine", "ejs");
 app.use("/public", express.static(path.join(__dirname, "static")));
 app.use("/peerjs", peerServer);
 
-// Routes
-const meetingRoutes = require("./routes/meetingRoutes.js");
-app.use("/", meetingRoutes);
-
-io.on("connection", (socket) => {
-  socket.on("join-room", (roomid, id, myname) => {
-    socket.join(roomid);
-    socket.to(roomid).broadcast.emit("user-connected", id, myname);
-
-    socket.on("disconnect", () => {
-      socket.to(roomid).broadcast.emit("user-disconnected", id);
-    });
-  });
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "static", "index.html"));
 });
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/join", (req, res) => {
+    res.redirect(
+        url.format({
+            pathname: `/join/${uuidv4()}`,
+            query: req.query,
+        })
+    );
+});
+
+app.get("/joinold", (req, res) => {
+    res.redirect(
+        url.format({
+            pathname: req.query.meeting_id,
+            query: req.query,
+        })
+    );
+});
+
+app.get("/join/:rooms", (req, res) => {
+    res.render("room", { roomid: req.params.rooms, Myname: req.query.name });
+});
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, id, myname) => {
+        socket.join(roomId);
+        socket.to(roomId).emit("user-connected", id, myname);
+
+
+
+        socket.on("tellName", (myname) => {
+            console.log(myname);
+            socket.to(roomId).emit("AddName", myname);
+
+        });
+
+        socket.on("disconnect", () => {
+            socket.to(roomId).emit("user-disconnected", id);
+        });
+    });
+});
+
+server.listen(PORT);
